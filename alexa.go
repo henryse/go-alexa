e6d4f8d28170adc4305221a536987c6af1c2e14f
Henry Seurer <henryse@gmail.com>
2019-01-01 23:19:51 -0800

    Update alexa.go

--- alexa.go
+++ alexa.go
@@ -1,5 +1,4 @@
 // **********************************************************************
-//    Copyright (c) 2018 Henry Seurer
 //
 //   Permission is hereby granted, free of charge, to any person
 //    obtaining a copy of this software and associated documentation
